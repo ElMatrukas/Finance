728d396179d4a1912373221a00931afc2917efa8
Matias Mendez <mati06mendez@gmail.com>
2025-08-03 22:43:34 -0300

    links, scrips and hmtl basic structure

---
+++ js/gasp.js
@@ -0,0 +1,8 @@
+<script>
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(Observer,ScrollTrigger,ScrollSmoother,ScrollToPlugin,SplitText,TextPlugin)
+  // gsap code here!
+ });
+
+</script>
